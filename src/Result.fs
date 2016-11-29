6e29a4ca4e29a7cc458d46a138303d2e96f58251
Pauan <pcxunlimited@gmail.com>
2016-11-29 04:21:47 -1000

    Adding in Result module

---
+++ src/Result.fs
@@ -0,0 +1,47 @@
+module Fable.PowerPack.Result
+
+
+type Result<'A, 'B> =
+| Success of 'A
+| Error of 'B
+
+
+let unwrapResult a =
+    match a with
+    | Success a -> a
+    | Error b -> raise b
+
+
+let map fn a =
+    match a with
+    | Success a -> Success (fn a)
+    | Error b -> Error b
+
+
+let bind fn a =
+    match a with
+    | Success a -> fn a
+    | Error b -> Error b
+
+
+// TODO implement TryFinally, TryWith, Using, While, and For ?
+type ResultBuilder() =
+    member this.Bind(m, f) = bind f m
+    member this.Return<'A, 'E>(a: 'A): Result<'A, 'E> = Success a
+    member this.ReturnFrom(m) = m
+    member this.Zero = this.Return
+
+    member this.Combine<'A, 'E>(left: Result<unit, 'E>, right: Result<'A, 'E>): Result<'A, 'E> =
+        this.Bind(left, fun () -> right)
+
+    (*member this.For<'A, 'E>(s: seq<'A>, fn: ('A -> Result<unit, 'E>)): Result<unit, 'E> =
+        let error = Seq.tryFind (fun a ->
+            match fn a with
+            | Success () -> false
+            | Error _ -> true) s
+
+        match error with
+        | Some e -> e
+        | None -> this.Zero()*)
+
+let result = new ResultBuilder()
