7c96e274e74ae6e4f729e0df47499d31b4c53860
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-03-29 10:32:26 +0200

    Replace Result type with F# 4.1 built-in

--- src/Result.fs
+++ src/Result.fs
@@ -1,35 +1,13 @@
 module Fable.PowerPack.Result
 
-
-// TODO replace this with F# 4.1 Result
-type Result<'A, 'B> =
-| Ok of 'A
-| Error of 'B
-
-
 let unwrapResult a =
     match a with
     | Ok a -> a
     | Error b -> raise b
 
-
-// TODO replace this with F# 4.1 map
-let map fn a =
-    match a with
-    | Ok a -> Ok (fn a)
-    | Error b -> Error b
-
-
-// TODO replace this with F# 4.1 bind
-let bind fn a =
-    match a with
-    | Ok a -> fn a
-    | Error b -> Error b
-
-
 // TODO implement TryFinally, TryWith, Using, While, and For ?
 type ResultBuilder() =
-    member this.Bind(m, f) = bind f m
+    member this.Bind(m, f) = Result.bind f m
     member this.Return<'A, 'E>(a: 'A): Result<'A, 'E> = Ok a
     member this.ReturnFrom(m) = m
     member this.Zero = this.Return
@@ -47,4 +25,4 @@ type ResultBuilder() =
         | Some e -> e
         | None -> this.Zero()*)
 
-let result = new ResultBuilder()
+let result = ResultBuilder()
