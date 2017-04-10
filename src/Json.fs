2f7ddf0e4d018dd482fdefa389bdf68e0d0a6193
Pauan <pcxunlimited@gmail.com>
2017-04-10 13:28:43 -1000

    Adding in safe statically-typed JSON parser

---
+++ src/Json.fs
@@ -0,0 +1,26 @@
+module Fable.PowerPack.Json
+
+open Fable.Core
+
+type Json =
+    | String of string
+    | Number of float
+    | Object of array<string * Json>
+    | Array of array<Json>
+    | Boolean of bool
+    | Null
+
+// TODO implement this in F# rather than the FFI
+[<Import("ofString", "./Json.js")>]
+let private _ofString:
+    (string -> Json) ->
+    (float -> Json) ->
+    (array<string * Json> -> Json) ->
+    (array<Json> -> Json) ->
+    (bool -> Json) ->
+    Json ->
+    (Json -> Result<Json, System.Exception>) ->
+    (System.Exception -> Result<Json, System.Exception>) ->
+    (string -> Result<Json, System.Exception>) = jsNative
+
+let ofString: string -> Result<Json, System.Exception> = _ofString String Number Object Array Boolean Null Ok Error
