2f16b5faa57e91075af8dadf78f91ee70340e62a
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-09-01 15:20:05 +0200

    Better tests output with describe

--- tests/FetchTests.fs
+++ tests/FetchTests.fs
@@ -15,57 +15,61 @@ let inline equal (expected: 'T) (actual: 'T): unit =
 [<Global>]
 let it (msg: string) (f: unit->JS.Promise<'T>): unit = jsNative
 
+[<Global>]
+let describe (msg: string) (f: unit->unit): unit = jsNative
+
 // Fetch polyfill for node
 JsInterop.importAll "isomorphic-fetch"
 
-it "fetch: requests work" <| fun () ->
-    let getWebPageLength url =
-        fetch url []
-        |> Promise.bind (fun res -> res.text())
-        |> Promise.map (fun txt -> txt.Length)
-    getWebPageLength "http://fable.io"
-    |> Promise.map (fun res -> res > 0 |> equal true)
+describe "Fetch tests" <| fun _ ->
+    it "fetch: requests work" <| fun () ->
+        let getWebPageLength url =
+            fetch url []
+            |> Promise.bind (fun res -> res.text())
+            |> Promise.map (fun txt -> txt.Length)
+        getWebPageLength "http://fable.io"
+        |> Promise.map (fun res -> res > 0 |> equal true)
 
-it "fetch: parallel requests work" <| fun () ->
-    let getWebPageLength url =
+    it "fetch: parallel requests work" <| fun () ->
+        let getWebPageLength url =
+            promise {
+                let! res = fetch url []
+                let! txt = res.text()
+                return txt.Length
+            }
+        [ "http://fable-compiler.github.io"
+        ; "http://babeljs.io"
+        ; "http://fsharp.org" ]
+        |> List.map getWebPageLength
+        |> Promise.Parallel
+        // The sum of lenghts of all web pages is
+        // expected to be bigger than 100 characters
+        |> Promise.map (fun results -> (Array.sum results) > 100 |> equal true)
+
+    it "fetch: unsuccessful HTTP status codes throw an error" <| fun () ->
         promise {
-            let! res = fetch url []
+            let! res = fetch "http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it" []
             let! txt = res.text()
-            return txt.Length
+            return txt
         }
-    [ "http://fable-compiler.github.io"
-    ; "http://babeljs.io"
-    ; "http://fsharp.org" ]
-    |> List.map getWebPageLength
-    |> Promise.Parallel
-    // The sum of lenghts of all web pages is
-    // expected to be bigger than 100 characters
-    |> Promise.map (fun results -> (Array.sum results) > 100 |> equal true)
+        |> Promise.catch (fun e -> e.Message)
+        |> Promise.map (fun results ->
+            results |> equal "404 Not Found for URL http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it")
 
-it "fetch: unsuccessful HTTP status codes throw an error" <| fun () ->
-    promise {
-        let! res = fetch "http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it" []
-        let! txt = res.text()
-        return txt
-    }
-    |> Promise.catch (fun e -> e.Message)
-    |> Promise.map (fun results ->
-        results |> equal "404 Not Found for URL http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it")
+    it "tryFetch: unsuccessful HTTP status codes returns an Error" <| fun () ->
+        tryFetch "http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it" []
+        |> Promise.map (fun a ->
+            match a with
+            | Ok a -> "failed"
+            | Error e -> e.Message)
+        |> Promise.map (fun results ->
+            results |> equal "404 Not Found for URL http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it")
 
-it "tryFetch: unsuccessful HTTP status codes returns an Error" <| fun () ->
-    tryFetch "http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it" []
-    |> Promise.map (fun a ->
-        match a with
-        | Ok a -> "failed"
-        | Error e -> e.Message)
-    |> Promise.map (fun results ->
-        results |> equal "404 Not Found for URL http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it")
-
-it "tryFetch: exceptions return an Error" <| fun () ->
-    tryFetch "http://this-must-be-an-invalid-url-no-really-i-mean-it.com" []
-    |> Promise.map (fun a ->
-        match a with
-        | Ok a -> "failed"
-        | Error e -> e.Message)
-    |> Promise.map (fun results ->
-        results |> equal "request to http://this-must-be-an-invalid-url-no-really-i-mean-it.com failed, reason: getaddrinfo ENOTFOUND this-must-be-an-invalid-url-no-really-i-mean-it.com this-must-be-an-invalid-url-no-really-i-mean-it.com:80")
+    it "tryFetch: exceptions return an Error" <| fun () ->
+        tryFetch "http://this-must-be-an-invalid-url-no-really-i-mean-it.com" []
+        |> Promise.map (fun a ->
+            match a with
+            | Ok a -> "failed"
+            | Error e -> e.Message)
+        |> Promise.map (fun results ->
+            results |> equal "request to http://this-must-be-an-invalid-url-no-really-i-mean-it.com failed, reason: getaddrinfo ENOTFOUND this-must-be-an-invalid-url-no-really-i-mean-it.com this-must-be-an-invalid-url-no-really-i-mean-it.com:80")
--- tests/JsonTests.fs
+++ tests/JsonTests.fs
@@ -16,36 +16,39 @@ let it (msg: string) (f: unit->unit): unit = jsNative
 [<Emit("new SyntaxError($0)")>]
 let SyntaxException (a: string): Exception = jsNative
 
+[<Global>]
+let describe (msg: string) (f: unit->unit): unit = jsNative
 
-it "Parsing works" <| fun () ->
-    ofString "{}" |> equal (Ok (Object [||]))
+describe "Json tests" <| fun _ ->
+    it "Parsing works" <| fun () ->
+        ofString "{}" |> equal (Ok (Object [||]))
 
-it "Parsing strings works" <| fun () ->
-    ofString "\"foo\"" |> equal (Ok (String "foo"))
+    it "Parsing strings works" <| fun () ->
+        ofString "\"foo\"" |> equal (Ok (String "foo"))
 
-it "Parsing integers works" <| fun () ->
-    ofString "50" |> equal (Ok (Number 50.0))
+    it "Parsing integers works" <| fun () ->
+        ofString "50" |> equal (Ok (Number 50.0))
 
-it "Parsing floats works" <| fun () ->
-    ofString "50.5" |> equal (Ok (Number 50.5))
+    it "Parsing floats works" <| fun () ->
+        ofString "50.5" |> equal (Ok (Number 50.5))
 
-it "Parsing booleans works" <| fun () ->
-    ofString "true" |> equal (Ok (Boolean true))
-    ofString "false" |> equal (Ok (Boolean false))
+    it "Parsing booleans works" <| fun () ->
+        ofString "true" |> equal (Ok (Boolean true))
+        ofString "false" |> equal (Ok (Boolean false))
 
-it "Parsing null works" <| fun () ->
-    ofString "null" |> equal (Ok Null)
+    it "Parsing null works" <| fun () ->
+        ofString "null" |> equal (Ok Null)
 
-it "Parsing objects works" <| fun () ->
-    ofString "{ \"foo\": null, \"bar\": 1.0, \"qux\": {} }" |> equal (Ok (Object [| ("foo", Null); ("bar", Number 1.0); ("qux", Object [||]) |]))
+    it "Parsing objects works" <| fun () ->
+        ofString "{ \"foo\": null, \"bar\": 1.0, \"qux\": {} }" |> equal (Ok (Object [| ("foo", Null); ("bar", Number 1.0); ("qux", Object [||]) |]))
 
-it "Parsing arrays works" <| fun () ->
-    ofString "[1, 2, 3]" |> equal (Ok (Array [| Number 1.0; Number 2.0; Number 3.0 |]))
+    it "Parsing arrays works" <| fun () ->
+        ofString "[1, 2, 3]" |> equal (Ok (Array [| Number 1.0; Number 2.0; Number 3.0 |]))
 
-it "Parsing bad input works" <| fun () ->
-    // TODO put the syntax error message
-    ofString "" |> equal (Error (SyntaxException ""))
+    it "Parsing bad input works" <| fun () ->
+        // TODO put the syntax error message
+        ofString "" |> equal (Error (SyntaxException ""))
 
-it "Parsing bad input works" <| fun () ->
-    // TODO put the syntax error message
-    ofString "{" |> equal (Error (SyntaxException ""))
+    it "Parsing bad input works" <| fun () ->
+        // TODO put the syntax error message
+        ofString "{" |> equal (Error (SyntaxException ""))
