5a603ba05607f4399e5735f5374d9d21207388ba
francois nicaise <whitetigle@gmail.com>
2018-04-16 12:16:31 +0200

    Added OPTIONS to Http methods + unit tests

--- tests/FetchTests.fs
+++ tests/FetchTests.fs
@@ -64,6 +64,28 @@ describe "Fetch tests" <| fun _ ->
         |> Promise.map (fun results ->
             results |> equal "404 Not Found for URL http://fable.io/this-must-be-an-invalid-url-no-really-i-mean-it")
 
+    it "tryFetch: Successful HTTP OPTIONS request" <| fun () ->
+        let successMessage = "OPTIONS request accepted (method allowed)"
+        let props = [ RequestProperties.Method HttpMethod.OPTIONS]
+        tryFetch "http://mockbin.org/bin/042d3a8c-edee-40ae-80f3-70ece6ae247e/view" props 
+        |> Promise.map (fun a ->
+            match a with
+            | Ok _ -> successMessage
+            | Error e -> e.Message)
+        |> Promise.map (fun results ->
+            results |> equal successMessage)
+
+    it "tryFetch: Failed HTTP OPTIONS request on Fable.io server" <| fun () ->
+        let failMessage = "OPTIONS request rejected (method not allowed)"
+        let props = [ RequestProperties.Method HttpMethod.OPTIONS]
+        tryFetch "http://fable.io" props 
+        |> Promise.map (fun a ->
+            match a with
+            | Ok a -> "ohoh? Fable.io allows OPTIONS request?"
+            | Error e -> failMessage)
+        |> Promise.map (fun results ->
+            results |> equal failMessage)
+
     it "tryFetch: exceptions return an Error" <| fun () ->
         tryFetch "http://this-must-be-an-invalid-url-no-really-i-mean-it.com" []
         |> Promise.map (fun a ->
